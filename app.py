a6569d4cb12eb32b9524b09bbe30a403b617d887
emrecncelik <emrecncelik@gmail.com>
2025-04-17 22:20:31 +0300

    st compat

--- app.py
+++ app.py
@@ -27,7 +27,9 @@ def get_credentials():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_config(
+                dict(st.secrets["gcloud"]["web"]), SCOPES
+            )
             creds = flow.run_local_server(port=8080)
 
         with open("token.json", "w") as token:
